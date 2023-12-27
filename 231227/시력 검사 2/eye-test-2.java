bf23d774aaf0b4bbd9e9854d7c628e1a9876d72e
정희석 <136571664+Bokman0201@users.noreply.github.com>
2023-12-27 16:57:11 +0900

    Title: 시력 검사 2, Time: 136ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 231227/시력 검사 2/eye-test-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double avg= sc.nextDouble();
+
+        if(avg>=1){
+            System.out.println("High");
+        }
+        else if(avg >=0.5){
+            System.out.println("Middle");
+        }
+        else{
+            System.out.println("Row");
+        }
+
+        
+
+    }
+}
