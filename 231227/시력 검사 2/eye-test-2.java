5df2c0a882e9c44fe2ce1a96f79941762a441d84
정희석 <136571664+Bokman0201@users.noreply.github.com>
2023-12-27 16:57:32 +0900

    Title: 시력 검사 2, Time: 138ms, Memory: 11MB, Status: Passed - CodeTree

--- 231227/시력 검사 2/eye-test-2.java
+++ 231227/시력 검사 2/eye-test-2.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println("Middle");
         }
         else{
-            System.out.println("Row");
+            System.out.println("Low");
         }
 
         
