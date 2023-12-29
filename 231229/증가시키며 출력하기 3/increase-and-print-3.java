6f936aefabf94298e834eb1b7ada78684016cc36
정희석 <136571664+Bokman0201@users.noreply.github.com>
2023-12-29 17:19:14 +0900

    Title: 증가시키며 출력하기 3, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = 10;
+
+        while(a<=26){
+            System.out.print(a+" ");
+            a+=2;
+        }
+    }
+}
